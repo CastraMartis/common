805eb7025152ceba528b1b9cbe47e0cbefcc74ad
scorbett123 <50634068+scorbett123@users.noreply.github.com>
2022-05-24 20:28:22 +0100

    Move math to common

---
+++ math/Vec3.kt
@@ -0,0 +1,3 @@
+package dev.scorbett123.common.math
+
+data class Vec3(val x: Float, val y: Float, val z: Float)
--- packets/PositionPacket.kt
+++ packets/PositionPacket.kt
@@ -2,7 +2,7 @@ package dev.scorbett123.common.packets
 
 import dev.scorbett123.common.utils.InputStream
 import dev.scorbett123.common.utils.OutputStream
-import dev.scorbett123.server.math.Vec3
+import dev.scorbett123.common.math.Vec3
 
 class PositionPacket(val x: Float, val y: Float, val z: Float): Packet() {
 
